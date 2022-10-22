c24c513f51fb8312830cdaec8fa5562ebf2bef05
FernandoLorca <florcapinto@gmail.com>
2022-10-22 15:46:08 -0300

    formating app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,22 +2,23 @@
 
 const inputNumber = document.querySelector('input[type="number"]');
 const inputColor = document.querySelector('input[type="color"]');
-let price = document.querySelector(
-  ".card__mainCard-precio > span"
-);
+let price = document.querySelector(".card__mainCard-precio > span");
 const quantity = document.querySelector(".card__mainCard-precio-cantidad");
 const colorChoose = document.querySelector(
   ".card__mainCard-colorElegido-color"
 );
 const button = document.querySelector(".card__mainCard-button");
 
-const originalPrice = price.textContent
+const originalPrice = price.textContent;
 
-button.addEventListener('click', () => {
-  price.textContent = originalPrice
+button.addEventListener("click", () => {
+  price.textContent = originalPrice;
 
-  inputNumber.value > 0 && inputNumber.value < 2 ? originalPrice : price.textContent = Number(price.textContent) * Number(inputNumber.value)
+  inputNumber.value > 0 && inputNumber.value < 2
+    ? originalPrice
+    : (price.textContent =
+        Number(price.textContent) * Number(inputNumber.value));
 
   quantity.textContent = inputNumber.value;
-  colorChoose.style.backgroundColor = inputColor.value
-})
+  colorChoose.style.backgroundColor = inputColor.value;
+});
